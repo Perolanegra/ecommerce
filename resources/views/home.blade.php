3570e8b3c01c4f48cb995597ed086cd17bd1fb06
Perolanegra <pedratto3@gmail.com>
2019-05-26 19:27:01 -0300

    Correcoes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    Você está logado!
                 </div>
             </div>
         </div>
